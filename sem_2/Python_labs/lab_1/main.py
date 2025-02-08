6ebe58eff07774f7b800e1cbc275880c80d7f4fc
Dmitriy <dimitruo@yandex.ru>
2025-02-08 23:16:17 +0300

    function completed

--- sem_2/Python_labs/lab_1/main.py
+++ sem_2/Python_labs/lab_1/main.py
@@ -1,40 +1,49 @@
 # Сложение и вычитание вещественных чисел в 4-й системе
 # счисления.
 
-def parts_num(num: str) -> (int, int):
-    num = num.strip(".")
-    return int(num[0]), int("0." + num[1])
+def parts_num(num: str) -> (int, float):
+    num = str(num).split(".")
+    if len(num) == 2:
+        return int(num[0]), float("0." + num[1])
+    else:
+        return int(num[0]), 0.0
 
 def converter_to_10(num_4: str) -> int:
-    num_4 = num_4.replace(".", " ")
+    point_ind = 0
+    for i in range(len(num_4)):
+        if num_4[i] == ".":
+            point_ind = i
+            break
+
+    cur_degree = point_ind - 1
+
+    num_4 = num_4.replace(".", "")
     num = 0
-    cur_degree = len(num_4) - 1
     for digit in num_4:
-        num += digit * 4**cur_degree
+        num += int(digit) * 4**cur_degree
         cur_degree -= 1
 
     return num
 
 def convert_from_10(num_n: int, n: int) -> str:
-    num_n = str(num_n.strip("."))
-
-    full_part, drob_part = parts_num(num_n)
+    full_part, drob_part = parts_num(str(num_n))
     num = ""
 
     while full_part:
-        num += full_part % n
+        num = str(full_part % n) + num
         full_part //= n
 
-    num += ","
+    if drob_part:
+        num += "."
 
-    countZero = 0
-    for _ in range(5):
-        drob_part *= n
-        full, drob = parts_num(drob_part)
+        for _ in range(5):
+            drob_part *= n
+            full, drob = parts_num(drob_part)
 
-        drob_part = drob
-        num += str(full)
+            drob_part = drob
+            num += str(full)
 
+    return num
 
 
 
