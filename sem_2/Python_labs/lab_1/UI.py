bc723887f2373046fff681557ac9a682d29b1658
Dmitriy <dimitruo@yandex.ru>
2025-02-12 06:38:30 +0300

    OPI lab_1 otchet add

--- sem_2/Python_labs/lab_1/UI.py
+++ sem_2/Python_labs/lab_1/UI.py
@@ -1,108 +1,154 @@
 import tkinter as tk
+from tkinter import messagebox
 from main import *
 
 
-
-def make_plus():
-    print("+")
-    n_1, n_2 = num_1.get(), num_2.get()
-    result.set(command_plus(n_1, n_2))
-
-def make_minus():
-    print("-")
-    n_1, n_2 = num_1.get(), num_2.get()
-    result.set(command_minus(n_1, n_2))
+def click_menu_about():
+    print("click about")
+    messagebox.showinfo(title="About", message="ООО \"ЛучшиеКалькуляторыВВидеПо\"\nДудырев Дмитрий ИУ7-22Б")
 
 def del_last():
-    if not selected_num.get():
-        num = num_1
-    else:
-        num = num_2
-    num.set(num.get()[:-1])
+    expression.set(expression.get()[:-1])
 
-def set_num(get_digit):
-    if not selected_num.get():
-        num = num_1
-    else:
-        num = num_2
-
-    num.set(num.get() + get_digit())
+def add_expression(get_digit):
+    expression.set(expression.get() + get_digit)
 
 def clear_all():
-    num_1.set("")
-    num_2.set("")
+    expression.set("")
     result.set("")
 
+def check_expression(*args):
+    exp = expression.get().strip()
+    if not exp:
+        return
+
+    correct_symbols = {"0", "1", "2", "3", "-", "+", " "}
+    if set(exp) - correct_symbols or not exp[0].isdigit() or not exp[-1].isdigit():
+        result_font_color.set("#B71C1C")
+        result_background_color.set("#FFCDD2")
+
+        result.set("Выражение не корректное")
+        correct_expression.set(False)
+    else:
+        result_font_color.set("#7AD948")
+        result_background_color.set("#CFFFB5")
+        result.set("")
+        correct_expression.set(True)
 
 
-def add_zero():
-    return "0"
+def solve_expression():
+    if not correct_expression.get():
+        return
 
-def add_one():
-    return "1"
+    cur_result = 0
+    minus = False
+    number = ""
+    for symbol in expression.get():
+        if symbol == "-":
+            if number:
+                cur_result += converter_to_10(number) * (-1 if minus else 1)
+                number = ""
+            minus = False if minus else True
+        elif symbol.isdigit():
+            number += symbol
+        elif number:
+            cur_result += converter_to_10(number) * (-1 if minus else 1)
+            number = ""
+            minus = False
 
-def add_two():
-    return "2"
+    if number:
+        cur_result += converter_to_10(number) * (-1 if minus else 1)
 
-def add_three():
-    return "3"
+    result.set(converter_from_10(cur_result, 4))
 
+# шаблок характеристики расположения кнопок
+SPEC = {
+    "ipadx": 60,
+    "ipady": 40,
+    "pady": 10,
+    "padx": 10
+}
 
+# характеристики расположения кнопок очистки
+SPEC_CLEAR = SPEC.copy()
+SPEC_CLEAR.update((
+    ("ipadx", 0),
+    ("ipady", 10)
+))
 
+# характеристики кнопок с числами и выражениями
+FUNC_BUTTON_STYLE = {
+    "font": (30)
+}
 
 window = tk.Tk()
-window.geometry("600x800")
+window.geometry("800x800")
+window.resizable(False, False)
 
-num_1 = tk.StringVar()
-num_2 = tk.StringVar()
+expression = tk.StringVar()
 result = tk.StringVar()
-selected_num = tk.BooleanVar(value=False)
+correct_expression = tk.BooleanVar(value=True)
+
+result_font_color = tk.StringVar(value="#B71C1C")
+result_background_color = tk.StringVar(value="#FFCDD2")
 
 
-for col in range(5):
+for col in range(4):
     window.columnconfigure(index=col, weight=1)
 for row in range(5):
     window.columnconfigure(index=row, weight=1)
 
 
 # Установка заголовков полей ввода и вывода
-tk.Label(text="Num_1:").grid(row=0, column=0)
-tk.Label(text="Num_2:").grid(row=1, column=0)
-tk.Label(text="Result:").grid(row=2, column=0)
-tk.Label(text="Clear:").grid(row=3, column=0)
-tk.Label(text="Operation:").grid(row=4, column=0)
-tk.Label(text="Selected num:").grid(row=5, column=0)
+tk.Label(text="Expression:").grid(row=0, column=0)
+# tk.Label(text="Num_2:").grid(row=1, column=0)
+tk.Label(text="Result:").grid(row=1, column=0)
+tk.Label(text="Clear:").grid(row=2, column=0)
+# tk.Label(text="Operation:").grid(row=4, column=0)
+# tk.Label(text="Selected expression:").grid(row=5, column=0)
 
-# Установка кнопок для выполнения арифметических операция
-tk.Button(text="+", command=make_plus).grid(row=4, column=1)
-tk.Button(text="-", command=make_minus).grid(row=4, column=2)
+
 # Установка кнопок для очиски полей
-tk.Button(text="Num_1", command=lambda: num_1.set("")).grid(row=3, column=1)
-tk.Button(text="Num_2", command=lambda: num_2.set("")).grid(row=3, column=2)
-tk.Button(text="Result", command=lambda: result.set("")).grid(row=3, column=3)
-tk.Button(text="All", command=clear_all).grid(row=3, column=4)
+tk.Button(text="Expression", command=lambda: expression.set("")).grid(row=2, column=1, **SPEC_CLEAR)
+# tk.Button(text="Num_2", command=lambda: num_2.set("")).grid(row=3, column=2)
+tk.Button(text="Result", command=lambda: result.set("")).grid(row=2, column=2, **SPEC_CLEAR)
+tk.Button(text="All", command=clear_all).grid(row=2, column=3, **SPEC_CLEAR)
 # Установка выбора изменяемго числа
-tk.Radiobutton(text="Num_1", value=False, variable=selected_num).grid(row=5, column=1, columnspan=2)
-tk.Radiobutton(text="Num_2", value=True, variable=selected_num).grid(row=5, column=2, columnspan=2)
-# Установка кнопок для изменения выбранного числа
-tk.Button(text="0", command=lambda: set_num(add_zero)).grid(row=6, column=0)
-tk.Button(text="1", command=lambda: set_num(add_one)).grid(row=6, column=1)
-tk.Button(text="2", command=lambda: set_num(add_two)).grid(row=6, column=2)
-tk.Button(text="3", command=lambda: set_num(add_three)).grid(row=6, column=3)
-tk.Button(text="del", command=del_last).grid(row=6, column=4)
+# tk.Radiobutton(text="Num_1", value=False, variable=selected_num).grid(row=5, column=1, columnspan=2)
+# tk.Radiobutton(text="Num_2", value=True, variable=selected_num).grid(row=5, column=2, columnspan=2)
+
+# Установка кнопок для ввода чисел
+tk.Button(text="0", command=lambda: add_expression("0"), **FUNC_BUTTON_STYLE).grid(row=5, column=0, **SPEC)
+tk.Button(text="1", command=lambda: add_expression("1"), **FUNC_BUTTON_STYLE).grid(row=5, column=1, **SPEC)
+tk.Button(text="2", command=lambda: add_expression("2"), **FUNC_BUTTON_STYLE).grid(row=5, column=2, **SPEC)
+tk.Button(text="3", command=lambda: add_expression("3"), **FUNC_BUTTON_STYLE).grid(row=5, column=3, **SPEC)
+
+tk.Button(text="=", command=solve_expression).grid(row=6, column=0, **SPEC)
+# Установка кнопок для ввода арифметических знаков
+tk.Button(text="+", command=lambda: add_expression(" + "), **FUNC_BUTTON_STYLE).grid(row=6, column=1, **SPEC)
+tk.Button(text="-", command=lambda: add_expression(" - "), **FUNC_BUTTON_STYLE).grid(row=6, column=2, **SPEC)
+# Установка кнопок для удаление последнего символа
+tk.Button(text="del", command=del_last, **FUNC_BUTTON_STYLE).grid(row=6, column=3, **SPEC)
 
 
 
 
-num_1_field = tk.Entry(textvariable=num_1)
-num_1_field.grid(row=0, column=1)
+expression_field = tk.Entry(textvariable=expression)
+expression_field.grid(row=0, column=1, columnspan=3)
 
-num_2_field = tk.Entry(textvariable=num_2)
-num_2_field.grid(row=1, column=1)
+expression.trace_add("write", check_expression)
+# num_2_field = tk.Entry(textvariable=num_2)
+# num_2_field.grid(row=1, column=1)
 
 result_field = tk.Label(textvariable=result)
-result_field.grid(row=2, column=1)
+result_field.grid(row=1, column=1, columnspan=3)
+
+main_menu = tk.Menu()
+main_menu.add_command(label="help")
+main_menu.add_command(label="About", command=click_menu_about)
 
 
 
+
+window.config(menu=main_menu)
 window.mainloop()
--- sem_2/Python_labs/lab_1/main.py
+++ sem_2/Python_labs/lab_1/main.py
@@ -2,37 +2,45 @@
 # счисления.
 
 def parts_num(num: str) -> (int, float):
-    num = str(num).split(".")
+    """
+    Функция принимает на вход число и возвращает его целую и дробную части
+    """
+    num = str(num).lstrip("-").split(".")
     if len(num) == 2:
         return int(num[0]), float("0." + num[1])
     else:
         return int(num[0]), 0.0
 
 def converter_to_10(num_4: str) -> int:
-    point_ind = 0
+    """
+    СС - система счисления
+    Функция принимает на вход строку в 4-й СС и возвращает число в 10-й СС
+    """
+    cur_degree = 0
     for i in range(len(num_4)):
         if num_4[i] == ".":
-            point_ind = i
             break
+        cur_degree = i
 
-    cur_degree = point_ind - 1
-
-    num_4 = num_4.replace(".", "")
     num = 0
     for digit in num_4:
-        num += int(digit) * 4**cur_degree
-        cur_degree -= 1
+        if digit != ".":
+
+            num += int(digit) * 4**cur_degree
+            cur_degree -= 1
 
     return num
 
-def convert_from_10(num_n: int, n: int) -> str:
+def converter_from_10(num_n: int, n: int) -> str:
     full_part, drob_part = parts_num(str(num_n))
     num = ""
 
+    # перевод целой части
     while full_part:
         num = str(full_part % n) + num
         full_part //= n
 
+    # перевеод дробной части
     if drob_part:
         num += "."
 
@@ -43,7 +51,19 @@ def convert_from_10(num_n: int, n: int) -> str:
             drob_part = drob
             num += str(full)
 
-    return num
+    return num if num_n >= 0 else "-" + num
+
+def command_plus(num_1: str, num_2: str) -> str:
+    "Функция принимает два числа и возвращает их сумму"
+    num_1 = converter_to_10(num_1)
+    num_2 = converter_to_10(num_2)
+    return converter_from_10(num_1 + num_2, 4)
+
+def command_minus(num_1: str, num_2: str) -> str:
+    "Функция принимает два числа и возвращает результат вычитания второго из первого"
+    num_1 = converter_to_10(num_1)
+    num_2 = converter_to_10(num_2)
+    return converter_from_10(num_1 - num_2, 4)
 
 
 
