02610e6d3675be3177d85234fb69141de61e077b
Dmitriy <dimitruo@yandex.ru>
2025-02-13 16:20:17 +0300

    fix func sum and subtract

--- sem_2/Python/lab_1/124.py
+++ sem_2/Python/lab_1/124.py
@@ -1,155 +1,70 @@
-def parts_num(num: str) -> (int, float):
-    """Функция принимает на вход число и возвращает его целую и дробную части"""
-    num = str(num).lstrip("-").split(".")
-    if len(num) == 2:
-        return int(num[0]), float("0." + num[1])
-    else:
-        return int(num[0]), 0.0
-
-def greeter_them(num_1: (str, str), num_2: (str, str)) -> bool:
-    # проверяем по длине целой части
-    if len(num_1[0]) > len(num_2[0]):
-        return True
-    else:
-        return False
-
-    # проверяем целую часть
-    for digit_1, digit_2 in zip(num_1[0], num_2[0]):
-        # print(digit_1, digit_2)
-        if digit_1 > digit_2:
-            return True
-        else:
-            return False
-
-    # проверяем дробную часть
-    for digit_1, digit_2 in zip(num_1[1], num_2[1], strict=True):
-        # print(digit_1, digit_2)
-        if digit_1 > digit_2:
-            return True
-        else:
-            return False
-
-    # длину дробной части
-    if len(num_1[1]) > len(num_2[1]):
-        return True
-    else:
-        return False
-
-
-
-
-def sum_in_4(num_1: str, num_2: str) -> str:
-    num_1 = num_1.split(".")
-    if len(num_1) == 1:
-        num_1.append("0")
-    num_2 = num_2.split(".")
-    if len(num_2) == 1:
-        num_2.append("0")
-
-
-    max_len_int = max(len(num_1[0]), len(num_2[0]))
-    max_len_drob = max(len(num_1[1]), len(num_2[1]))
-
-    num_1, num_2 = map(lambda x: (x[0].rjust(max_len_int, "0"), x[1].ljust(max_len_drob, "0")), (num_1, num_2))
-
-    new_num = ""
-    overflow = 0
-
-    for i in range(max_len_drob - 1, -1, -1):
-        digit_1 = int(num_1[1][i])
-        digit_2 = int(num_2[1][i])
-
-        digit = digit_1 + digit_2 + overflow
-        overflow = 0
-        if digit >= 4:
-            overflow = 1
-            digit -= 4
-
-        new_num = str(digit) + new_num
-
-    new_num = "." + new_num
-
-    for i in range(max_len_int - 1, -1, -1):
-        digit_1 = int(num_1[0][i])
-        digit_2 = int(num_2[0][i])
-
-        digit = digit_1 + digit_2 + overflow
-        overflow = 0
-        if digit >= 4:
-            overflow = 1
-            digit -= 4
-
-        new_num = str(digit) + new_num
-
-    if overflow:
-        new_num = str(overflow) + new_num
-    return new_num
-
-def substract_in_4(num_1: str, num_2: str) -> str:
-    num_1 = num_1.split(".")
-    if len(num_1) == 1:
-        num_1.append("0")
-    num_2 = num_2.split(".")
-    if len(num_2) == 1:
-        num_2.append("0")
-
-    is_positive_num = greeter_them(num_1, num_2)
-    if not is_positive_num:
-        num_1, num_2 = num_2, num_1
-
-    max_len_int = max(len(num_1[0]), len(num_2[0]))
-    max_len_drob = max(len(num_1[1]), len(num_2[1]))
-
-    num_1, num_2 = map(lambda x: (x[0].rjust(max_len_int, "0"), x[1].ljust(max_len_drob, "0")), (num_1, num_2))
-
-    new_num = ""
-    overflow = False
-
-    for i in range(max_len_drob - 1, -1, -1):
-        digit_1 = int(num_1[1][i])
-        digit_2 = int(num_2[1][i])
-
-        digit = digit_1 - digit_2
-
-        if overflow:
-            digit -= 1
-            overflow = False
-
-        if digit < 0:
-            overflow = True
-            digit += 4
-
-        new_num = str(digit) + new_num
-
-    new_num = "." + new_num
-
-    for i in range(max_len_int - 1, -1, -1):
-        digit_1 = int(num_1[0][i])
-        digit_2 = int(num_2[0][i])
-
-        digit = digit_1 - digit_2
-
-        if overflow:
-            digit -= 1
-            overflow = False
-
-        if digit < 0:
-            overflow = True
-            digit += 4
-
-        new_num = str(digit) + new_num
-
-    # if overflow:
-    #     new_num = str(overflow) + new_num
-
-    return new_num if is_positive_num else "-" + new_num
-
-
-print(sum_in_4("23.1", "322.1"))
-print(substract_in_4("2", "21"))
-print(substract_in_4("21", "2"))
+# def str_to_float(num_str, base=10):
+#     if '.' not in num_str:
+#         return float(int(num_str, base))
 #
-# 02
-# 21
+#     integer_part, fractional_part = num_str.split('.')
+#
+#     # Целая часть
+#     integer = int(integer_part, base)
+#
+#     # Дробная часть
+#     fractional = 0.0
+#     for i, char in enumerate(fractional_part, 1):
+#         digit = int(char, base)
+#         fractional += digit * (base ** -i)
+#
+#     return integer + fractional
 
 
+
+
+
+# import random
+#
+# symbols = "0123"
+# numbers_1 = []
+# numbers_2 = []
+# for _ in range(100_000):
+#     l_1 = random.randint(2, 10)
+#     num_1 = "".join(random.choices(symbols, k=l_1))
+#     while num_1[0] == "0":
+#         num_1 = "".join(random.choices(symbols, k=random.randint(2, 5)))
+#     if random.randint(0, 10) > 8:
+#         i = random.randint(1, l_1 - 1)
+#         num_1 = num_1[:i] + "." + num_1[i:]
+#     numbers_1.append(num_1)
+#
+#     l_2 = random.randint(2, 10)
+#     num_2 = "".join(random.choices(symbols, k=l_2))
+#     index_point_1 = random.randint(1, l_1 - 1)
+#     while num_2[0] == "0":
+#         num_2 = "".join(random.choices(symbols, k=random.randint(2, 5)))
+#     if random.randint(0, 10) > 8:
+#         i = random.randint(1, l_1 - 1)
+#         num_2 = num_2[:i] + "." + num_2[i:]
+#     numbers_2.append(num_2)
+#
+# for num_1, num_2 in zip(numbers_1, numbers_2):
+#     num_1_10 = str_to_float(num_1, 4)
+#     num_2_10 = str_to_float(num_2, 4)
+#     s_10 = num_1_10 + num_2_10
+#     d_10 = num_1_10 - num_2_10
+#
+#     s_4 = sum_in_4(num_1, num_2)
+#     d_4 = substract_in_4(num_1, num_2)
+#     # print(f"{num_1} + {num_2} = {s_4}")
+#     # print(f"{num_1} - {num_2} = {d_4}")
+#
+#     if not(s_10 == str_to_float(s_4, 4)):
+#         print("Короче сумма не работает")
+#         print(num_1, num_2, s_4)
+#         break
+#     if not (d_10 == str_to_float(d_4, 4)):
+#         print(d_10)
+#         print("Короче разница не работает")
+#         print(num_1, num_2, d_4)
+#         break
+#
+#
+# print(substract_in_4("3", "3"))
+# # 21.30 31133 -31111.10
--- sem_2/Python/lab_1/main.py
+++ sem_2/Python/lab_1/main.py
@@ -1,70 +1,141 @@
 # Сложение и вычитание вещественных чисел в 4-й системе счисления.
 
-
 def parts_num(num: str) -> (int, float):
     """Функция принимает на вход число и возвращает его целую и дробную части"""
     num = str(num).lstrip("-").split(".")
     if len(num) == 2:
-        return int(num[0]), float("0." + num[1])
+        return num[0], num[1]
     else:
-        return int(num[0]), 0.0
+        return num[0], 0
 
-def converter_to_10(num_4: str) -> int:
-    """
-    СС - система счисления
-    Функция принимает на вход строку в 4-й СС и возвращает число в 10-й СС
-    """
-    cur_degree = 0
-    for i in range(len(num_4)):
-        if num_4[i] == ".":
-            break
-        cur_degree = i
+def greeter_them(num_1: (str, str), num_2: (str, str)) -> bool:
+    # проверяем по длине целой части
+    if len(num_1[0]) > len(num_2[0]):
+        return True
+    elif len(num_1[0]) < len(num_2[0]):
+        return False
 
-    num = 0
-    for digit in num_4:
-        if digit != ".":
+    # проверяем целую часть
+    for digit_1, digit_2 in zip(num_1[0], num_2[0]):
+        # print(digit_1, digit_2)
+        if digit_1 > digit_2:
+            return True
+        elif digit_1 < digit_2:
+            return False
 
-            num += int(digit) * 4**cur_degree
-            cur_degree -= 1
+    # проверяем дробную часть
+    for digit_1, digit_2 in zip(num_1[1], num_2[1], strict=True):
+        # print(digit_1, digit_2)
+        if digit_1 > digit_2:
+            return True
+        elif digit_1 < digit_2:
+            return False
 
-    return num
+    # длину дробной части
+    if len(num_1[1]) > len(num_2[1]):
+        return True
+    elif len(num_1[1]) < len(num_2[1]):
+        return False
 
-def converter_from_10(num_n: int, n: int) -> str:
-    """Переводитьь число из 10 системы счисления в n-ю систему счисления"""
-    full_part, drob_part = parts_num(str(num_n))
-    num = ""
-
-    # перевод целой части
-    while full_part:
-        num = str(full_part % n) + num
-        full_part //= n
-
-    # перевеод дробной части
-    if drob_part:
-        num += "."
-
-        for _ in range(5):
-            drob_part *= n
-            full, drob = parts_num(drob_part)
-
-            drob_part = drob
-            num += str(full)
-
-    return num.rstrip("0") if num_n >= 0 else "-" + num.rstrip("0")
-
-def command_plus(num_1: str, num_2: str) -> str:
-    "Функция принимает два числа и возвращает их сумму"
-    num_1 = converter_to_10(num_1)
-    num_2 = converter_to_10(num_2)
-    return converter_from_10(num_1 + num_2, 4)
-
-def command_minus(num_1: str, num_2: str) -> str:
-    "Функция принимает два числа и возвращает результат вычитания второго из первого"
-    num_1 = converter_to_10(num_1)
-    num_2 = converter_to_10(num_2)
-    return converter_from_10(num_1 - num_2, 4)
+    # числа равны
+    return True
 
 
+def sum_in_4(num_1: str, num_2: str) -> str:
+    num_1 = parts_num(num_1)
+    num_2 = parts_num(num_2)
 
+    max_len_int = max(len(num_1[0]), len(num_2[0]))
+    max_len_drob = max(len(num_1[1]), len(num_2[1]))
+
+    num_1, num_2 = map(lambda x: (x[0].rjust(max_len_int, "0"), x[1].ljust(max_len_drob, "0")), (num_1, num_2))
+
+    new_num = ""
+    overflow = 0
+
+    for i in range(max_len_drob - 1, -1, -1):
+        digit_1 = int(num_1[1][i])
+        digit_2 = int(num_2[1][i])
+
+        digit = digit_1 + digit_2 + overflow
+        overflow = 0
+        if digit >= 4:
+            overflow = 1
+            digit -= 4
+
+        new_num = str(digit) + new_num
+
+    new_num = "." + new_num
+
+    for i in range(max_len_int - 1, -1, -1):
+        digit_1 = int(num_1[0][i])
+        digit_2 = int(num_2[0][i])
+
+        digit = digit_1 + digit_2 + overflow
+        overflow = 0
+        if digit >= 4:
+            overflow = 1
+            digit -= 4
+
+        new_num = str(digit) + new_num
+
+    if overflow:
+        new_num = str(overflow) + new_num
+    return new_num.strip("0").rstrip(".")
+
+
+def substract_in_4(num_1: str, num_2: str) -> str:
+    num_1 = parts_num(num_1)
+    num_2 = parts_num(num_2)
+
+    is_positive_num = greeter_them(num_1, num_2)
+    if not is_positive_num:
+        num_1, num_2 = num_2, num_1
+
+    max_len_int = max(len(num_1[0]), len(num_2[0]))
+    max_len_drob = max(len(num_1[1]), len(num_2[1]))
+
+    num_1, num_2 = map(lambda x: (x[0].rjust(max_len_int, "0"), x[1].ljust(max_len_drob, "0")), (num_1, num_2))
+
+    new_num = ""
+    overflow = False
+
+    for i in range(max_len_drob - 1, -1, -1):
+        digit_1 = int(num_1[1][i])
+        digit_2 = int(num_2[1][i])
+
+        digit = digit_1 - digit_2
+
+        if overflow:
+            digit -= 1
+            overflow = False
+
+        if digit < 0:
+            overflow = True
+            digit += 4
+
+        new_num = str(digit) + new_num
+
+    new_num = "." + new_num
+
+    for i in range(max_len_int - 1, -1, -1):
+        digit_1 = int(num_1[0][i])
+        digit_2 = int(num_2[0][i])
+
+        digit = digit_1 - digit_2
+
+        if overflow:
+            digit -= 1
+            overflow = False
+
+        if digit < 0:
+            overflow = True
+            digit += 4
+
+        new_num = str(digit) + new_num
+
+    if not is_positive_num:
+        new_num = "-" + new_num
+    return new_num.strip("0").rstrip(".")
 
 
