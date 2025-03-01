55af72737d81f81aaa7a45a0208f517cea5a77f0
Dmitriy Dudyrev <dmitriy@h160.net39.bmstu.ru>
2025-03-01 09:55:50 +0300

    update scriprts_template

---
+++ sem_2/C/lab_works/dir_template/comparator_type/sub_string.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function get_sub_string
+{
+    # если присутсвует только одно вхождение подстроки
+    cat $1 | sed "s/.*""$sub_string""[[:space:]]*//"
+
+    # # если присутсвует несколько вхождение подстроки
+    # echo $string | sed "s/"$sub_string"[[:space:]]*/\n/; s/.*\n//"
+}
+
+sub_string="Result: "
+
+content_1=$(get_sub_string $1)
+# echo $content_1
+content_2=$(get_sub_string $2)
+# echo $content_2
+
+if [ "$content_1" = "$content_2" ]; then
+    echo 0
+else
+    echo 1
+fi
