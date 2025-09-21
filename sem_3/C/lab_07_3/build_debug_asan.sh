ef217351e6427afed8343ac2fa99ff8c17417d1f
Dmitriy <dimitruo@yandex.ru>
2025-09-21 22:48:35 +0300

    new struct of project

--- sem_3/C/lab_07_3/build_debug_asan.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-clang -c ./*.c -Wall -Werror -Wextra -Wpedantic -std=c99 -Wfloat-equal -Wfloat-conversion -Wvla -fsanitize=address -fno-omit-frame-pointer -g
-
-clang ./*.o -fsanitize=address -o app.exe -lm
--- sem_3/C/lab_07_3/collect_coverage.sh
+++ sem_3/C/lab_07_3/collect_coverage.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-start_directory=$(pwd)
-while ! (pwd | grep -qE "lab(_[0-9][0-9]){2,3}$"); do
-    # pwd
-    cd .. || exit
-done
-
-
-./build_gcov.sh
-
 ./func_tests/scripts/func_tests.sh -q
 
 gcov ./*.gcda
@@ -17,8 +8,3 @@ if [ "$1" = "-v" ]; then
     cat ./*.c.gcov
     echo
 fi
-
-./clean.sh
-
-
-cd "$start_directory" || exit
