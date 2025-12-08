afa8410b3ea0865d0b60e7baf1c192d6f43b7cc1
Dmitriy <dimitruo@yandex.ru>
2025-12-08 08:28:04 +0300

    add enum with error code

---
+++ sem_3/Tisd/lab_07/inc/exit_code.h
@@ -0,0 +1,14 @@
+#ifndef EXIT_CODE_H__
+#define EXIT_CODE_H__
+
+typedef enum 
+{
+    OK,
+    INVALID_INPUT,
+    STR_OVERFLOW,
+    STR_EMPTY,
+    VALUE_EXITS,
+    VALUE_NOT_EXITS,
+} error;
+
+#endif
