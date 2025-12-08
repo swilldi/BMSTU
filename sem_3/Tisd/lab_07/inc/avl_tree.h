65822c2524b124a2be63d4e22af469e94f75d1ff
Dmitriy <dimitruo@yandex.ru>
2025-12-08 13:00:10 +0300

    add func for avl tree

---
+++ sem_3/Tisd/lab_07/inc/avl_tree.h
@@ -0,0 +1,55 @@
+#ifndef AVL_TREE_H__
+#define AVL_TREE_H__
+
+#include "common_define.h"
+
+typedef struct avl_tree_node avl_tree_node;
+typedef void (*avl_tree_apply_func)(avl_tree_node *node, void *param);
+
+// === Выделение и освобождение памяти ===
+// Создание дерева
+avl_tree_node *avl_tree_create(void);
+
+// выделение памяти для узла дерева
+avl_tree_node *avl_tree_node_create(char *value);
+// освобождение памяти узла дерева
+void avl_tree_node_destroy(avl_tree_node *node, void *param);
+void avl_tree_free_node_content(avl_tree_node *node, void *param);
+
+// освобождение памяти всего дерева
+// void avl_tree_destroy(avl_tree_node *avl_tree, avl_tree_apply_func free_func)
+void avl_tree_destroy(avl_tree_node *avl_tree);
+
+
+// === Приминение ко всем элементам дерева ===
+// префиксное приминение функции func ко всем узлам
+void avl_tree_apple_pre(avl_tree_node *avl_tree, avl_tree_apply_func func, void *param);
+// инфиксное приминение функции func ко всем узлам
+void avl_tree_apple_in(avl_tree_node *avl_tree, avl_tree_apply_func func, void *param);
+// постфиксное приминение функции func ко всем узлам
+void avl_tree_apple_post(avl_tree_node *avl_tree, avl_tree_apply_func func, void *param);
+
+// === Взаимодействие с деревом ===
+// Вставка нового узла node
+avl_tree_node *avl_tree_insert(avl_tree_node *avl_tree, avl_tree_node *node, compare_func cmp_func);
+
+// Удаление узла со значением value
+avl_tree_node *avl_tree_remove(avl_tree_node *avl_tree, char *value, compare_func cmp_func);
+
+// === Поиск элементов ===
+// Поиск узла со значеним value
+avl_tree_node *avl_tree_lookup(avl_tree_node *avl_tree, char *value, compare_func cmp_func);
+// Поиск родителя узла node
+avl_tree_node *avl_tree_lookup_parent(avl_tree_node *avl_tree, avl_tree_node *node, compare_func cmp_func);
+
+// Поиск минимального узал в дереве avl_tree
+avl_tree_node *avl_tree_pop_min(avl_tree_node **avl_tree);
+
+
+// === Доступ к данным ===
+void *avl_tree_value(avl_tree_node *node);
+avl_tree_node *avl_tree_left_child(avl_tree_node *node);
+
+avl_tree_node *avl_tree_right_child(avl_tree_node *node);
+
+#endif
---
+++ sem_3/Tisd/lab_07/src/avl_tree.c
@@ -0,0 +1,305 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "avl_tree.h"
+
+struct avl_tree_node
+{
+    // хранимые данные
+    char value[TREE_STR_LEN];
+
+    // Дочерние ноды
+    avl_tree_node *left;
+    avl_tree_node *right;
+
+    // Парамет сбалансированности
+    int height;
+};
+
+// === Выделение и освобождение памяти ===
+// Создание дерева
+avl_tree_node *avl_tree_create(void)
+{
+    return NULL;
+}
+
+// выделение памяти для узла дерева
+avl_tree_node *avl_tree_node_create(char *value)
+{
+    avl_tree_node *node = malloc(sizeof(avl_tree_node));
+    if (node)
+    {
+        node->left = node->right = NULL;
+        strcpy(node->value, value);
+    }
+    return node;
+}
+// освобождение памяти узла дерева
+void avl_tree_node_destroy(avl_tree_node *node, void *param)
+{   
+    (void)param;
+    free(node);
+}
+
+// void avl_tree_free_node_content(avl_tree_node *node, void *param)
+// {   
+//     (void)param;
+//     free(node->value);
+// }
+
+// освобождение памяти всего дерева
+// void avl_tree_destroy(avl_tree_node *avl_tree, avl_tree_apply_func free_func)
+void avl_tree_destroy(avl_tree_node *avl_tree)
+{
+    if (!avl_tree)
+        return;
+
+    // Очищение содержимого дерева
+    avl_tree_apple_post(avl_tree, avl_tree_free_node_content, NULL);
+    // Освобождение памяти узлов дерева
+    avl_tree_apple_post(avl_tree, avl_tree_node_destroy, NULL);
+}
+
+
+// === Приминение ко всем элементам дерева ===
+// префиксное приминение функции func ко всем узлам
+void avl_tree_apple_pre(avl_tree_node *avl_tree, avl_tree_apply_func func, void *param)
+{
+    if (!avl_tree)
+        return;
+
+    func(avl_tree, param);
+    avl_tree_apple_pre(avl_tree->left, func, param);
+    avl_tree_apple_pre(avl_tree->right, func, param);
+}
+// инфиксное приминение функции func ко всем узлам
+void avl_tree_apple_in(avl_tree_node *avl_tree, avl_tree_apply_func func, void *param)
+{
+    if (!avl_tree)
+        return;
+
+    avl_tree_apple_in(avl_tree->left, func, param);
+    func(avl_tree, param);
+    avl_tree_apple_in(avl_tree->right, func, param);
+}
+// постфиксное приминение функции func ко всем узлам
+void avl_tree_apple_post(avl_tree_node *avl_tree, avl_tree_apply_func func, void *param)
+{
+    if (!avl_tree)
+        return;
+    
+    avl_tree_apple_post(avl_tree->left, func, param);
+    avl_tree_apple_post(avl_tree->right, func, param);
+    func(avl_tree, param);
+}
+
+// === Взаимодействие с деревом ===
+// Вставка нового узла node
+avl_tree_node *avl_tree_insert(avl_tree_node *avl_tree, avl_tree_node *node, compare_func cmp_func)
+{
+    if (!avl_tree)
+        return node;
+
+    int cmp = cmp_func(avl_tree->value, node->value);
+    if (cmp > 0)
+    {
+        avl_tree->left = avl_tree_insert(avl_tree->left, node, cmp_func);
+        avl_tree->left = avl_tree_balanace(avl_tree->left);
+    }
+    else if (cmp < 0)
+    {
+        avl_tree->right = avl_tree_insert(avl_tree->right, node, cmp_func);
+        avl_tree->right = avl_tree_balanace(avl_tree->right);
+    }
+    return avl_tree;
+}
+
+// Удаление узла со значением value
+avl_tree_node *avl_tree_remove(avl_tree_node *avl_tree, char *value, compare_func cmp_func)
+{
+    if (!avl_tree)
+        return NULL;
+
+    int cmp = cmp_func(avl_tree->value, value);
+    if (cmp > 0)
+    {
+        avl_tree->left = avl_tree_remove(avl_tree->left, value, cmp_func);
+        avl_tree->left = avl_tree_balanace(avl_tree->left);
+    }
+    else if (cmp < 0)
+    {
+        avl_tree->right = avl_tree_remove(avl_tree->right, value, cmp_func);
+        avl_tree->right = avl_tree_balanace(avl_tree->right);
+    }
+    else
+    {
+        if (avl_tree->left == NULL)
+        {
+            avl_tree_node *temp_node = avl_tree->right;
+            free(avl_tree);
+            return temp_node;
+        }
+        else if (avl_tree->right == NULL)
+        {
+            avl_tree_node *temp_node = avl_tree->left;
+            free(avl_tree);
+            return temp_node;
+        }
+        else
+        {
+            avl_tree_node *min_node = avl_tree_pop_min(&avl_tree->right);
+            min_node->left = avl_tree->left;
+            min_node->right = avl_tree->right;
+            free(avl_tree);
+            return min_node;
+        }
+    }
+
+    return avl_tree;
+}
+
+// === Поиск элементов ===
+// Поиск узла со значеним value
+avl_tree_node *avl_tree_lookup(avl_tree_node *avl_tree, char *value, compare_func cmp_func)
+{
+    int cmp;
+
+    while (avl_tree != NULL)
+    {
+        cmp = cmp_func(avl_tree->value, value);
+        if (cmp > 0)
+            avl_tree = avl_tree->left;
+        else if (cmp < 0)
+            avl_tree = avl_tree->right;
+        else
+            return avl_tree;
+    }
+
+    return NULL;
+}
+// Поиск родителя узла node
+avl_tree_node *avl_tree_lookup_parent(avl_tree_node *avl_tree, avl_tree_node *node, compare_func cmp_func)
+{
+    int cmp;
+
+    while (avl_tree != NULL)
+    {
+        cmp = cmp_func(avl_tree->value, node->value);
+        if (cmp > 0)
+            avl_tree = avl_tree->left;
+        else if (cmp < 0)
+            avl_tree = avl_tree->right;
+        else if (avl_tree->left == node || avl_tree->right == node)
+            return avl_tree;
+        else
+            return NULL;
+    }
+
+    return NULL;
+}
+
+// Поиск минимального узал в дереве avl_tree
+avl_tree_node *avl_tree_pop_min(avl_tree_node **avl_tree)
+{
+    avl_tree_node *cur = *avl_tree, *prev = NULL;
+    while (cur->left)
+    {
+        prev = cur;
+        cur = cur->left;
+    }
+    
+    if (!prev)
+        *avl_tree = NULL;
+    else
+        prev->left = NULL;
+    
+    return cur;
+}
+
+
+// === Доступ к данным ===
+void *avl_tree_value(avl_tree_node *node)
+{
+    return node->value;
+}
+
+avl_tree_node *avl_tree_left_child(avl_tree_node *node)
+{
+    return node->left;
+}
+
+avl_tree_node *avl_tree_right_child(avl_tree_node *node)
+{
+    return node->right;
+}
+
+avl_tree_node *avl_tree_height(avl_tree_node *node)
+{
+    return node ? node->height : 0;
+}
+
+// === Балансировка ===
+// Изменение состояние балансировки
+void avl_tree_set_height(avl_tree_node *node)
+{
+    if (!node)
+        return 0;
+
+    int lh = avl_tree_height((node->left));
+    int lr = avl_tree_height((node->right));
+
+    return (lh > lr ? lh : lr) + 1;
+}
+
+// Состояние баналанса
+int avl_tree_balance_factor(avl_tree_node *node)
+{
+    return avl_tree_height(node->right) - avl_tree_height(node->left);
+}
+
+// Левый поворот
+avl_tree_node *avl_tree_rotate_right(avl_tree_node *node)
+{
+    avl_tree_node *temp_node = node->left;
+    node->left = temp_node->right;
+    temp_node->right = node;
+    avl_tree_set_height(node);
+    avl_tree_set_height(temp_node);
+    return temp_node;   
+}
+// Правый поворот
+avl_tree_node *avl_tree_rotate_left(avl_tree_node *node)
+{
+    avl_tree_node *temp_node = node->right;
+    node->right = temp_node->left;
+    temp_node->left = node;
+    avl_tree_set_height(node);
+    avl_tree_set_height(temp_node);
+    return temp_node;
+}
+
+avl_tree_node *avl_tree_balanace(avl_tree_node *node)
+{
+    avl_tree_set_height(node);
+
+    // расбалансированно справа
+    if (avl_tree_balance_factor(node) > 1)
+    {
+        if (avl_tree_balance_factor(node->right) < 0)
+            node->right = avl_tree_rotate_right(node->right);
+        
+        node = avl_tree_rotate_left(node);
+    }
+
+    // расбалансирована слева
+    if (avl_tree_balance_factor(node) < -1)
+    {
+        if (avl_tree_balance_factor(node->left) > 0)
+            node->left = avl_tree_rotate_left(node->left);
+        
+        node = avl_tree_rotate_right(node);
+    }
+
+    // дерево сбалансированно 
+    return node;
+}
