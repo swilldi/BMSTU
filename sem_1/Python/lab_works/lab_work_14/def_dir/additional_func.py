14c96ee0ebe33bd51f75731e4f8dd39166e90e6a
Dmitriy <dimitruo@yandex.ru>
2026-01-20 21:46:39 +0300

    sem_1 cleared

--- sem_1/Python/lab_works/lab_work_14/def_dir/additional_func.py
+++
@@ -1,8 +0,0 @@
-def generate_bin_number(file: str, n: int, /, negative: bool =True):
-    with open(file, "wb+") as f:
-        for _ in range(n):
-            if negative:
-                num = randint(-10**2, 10**2)
-            else:
-                num = randint(1, 10**2)
-            f.write(pack(FMT, int(num)))
--- sem_1/Python/lab_works/lab_work_15/test.py
+++
@@ -1,5 +0,0 @@
-import os
-from bin_file_func import *
-
-print(10 % 4 )
-
