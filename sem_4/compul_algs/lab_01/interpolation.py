a35397bae85aeaf01c20ddd42abf894f076e1ee9
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-03-02 15:02:15 +0300

    Фиксы вычислительной части

--- sem_4/compul_algs/lab_01/interpolation.py
+++ sem_4/compul_algs/lab_01/interpolation.py
@@ -1,108 +1,85 @@
 from math import ceil
 
+
 def index_point(table: list[tuple[float, float]], target_x: float) -> int:
     ind = 0
-    diff = table[0][0] - target_x
 
     # поиск первого числа больше target_x
-    while diff < 0:
+    while ind + 1 < len(table) and table[ind][0] < target_x:
         ind += 1
-        diff = table[ind][0] - target_x
 
-    # проверка предыдущего значени
+    # проверка наибольшего числа, которое меньше target_x
     if ind > 0:
+        cur_diff = abs(table[ind][0] - target_x)
         prev_diff = abs(table[ind - 1][0] - target_x)
-        if prev_diff < diff:
+        if prev_diff < cur_diff:
             ind -= 1
 
     return ind
 
-def environment_of_point(table: list[tuple[float, float]], target_x: float, n: int) -> list[tuple[float, float]]:
+
+def environment_of_point(
+    table: list[tuple[float, float]], target_x: float, points_count: int
+) -> list[tuple[float, float]]:
     central_index = index_point(table, target_x)
 
-    start_ind = central_index - n // 2
-    end_ind = central_index + ceil(n / 2)
+    start_ind = central_index - points_count // 2
+    end_ind = central_index + ceil(points_count / 2)
 
     if start_ind < 0:
-        end_ind += abs(start_ind)
+        end_ind += -start_ind
         start_ind = 0
-    elif end_ind >= len(table):
+
+    if end_ind > len(table):
         start_ind -= end_ind - len(table)
         end_ind = len(table)
 
+    start_ind = max(start_ind, 0)
     return table[start_ind:end_ind]
 
-def divided_difference(x1, x2, y1, y2):
+
+def divided_difference(x1: float, x2: float, y1: float, y2: float) -> float:
     return (y1 - y2) / (x1 - x2)
 
 
-def newton_polynomial(table: list[tuple[float, float]], target_x: float, n: int) -> float:
-    '''
-    :param table: (x, y)
-    :param target_x:
-    :param n: степень полина
-    :return: значение функции в точке x
-    '''
-
-    # вычисление коэфициентов
-    env = environment_of_point(table, target_x, n + 1)
+def newton_polynomial(
+    table: list[tuple[float, float]], target_x: float, degree: int
+) -> float:
+    degree = min(degree, len(table) - 1)
+    env = environment_of_point(table, target_x, degree + 1)
     coef = [env[0][1]]
 
-    y_values = [y for x, y in env]
-    for i in range(n, 0, -1):
-        x_ind_shift = n + 1 - i
+    y_values = [y for _, y in env]
+    for i in range(degree, 0, -1):
+        x_ind_shift = degree + 1 - i
         for j in range(i):
             x1, x2 = env[j][0], env[j + x_ind_shift][0]
             y1, y2 = y_values[j], y_values[j + 1]
             y_values[j] = divided_difference(x1, x2, y1, y2)
         coef.append(y_values[0])
-        print(f"[{i}] -> {y_values[:i]}")
 
-    # Вычисление полинома
-    x, y = 1, 0
+    x = 1.0
+    y = 0.0
     for i in range(len(env)):
         y += coef[i] * x
-        x *= (target_x - env[i][0])
+        x *= target_x - env[i][0]
 
     return y
 
-def newton_polynomial2D(table: list[tuple[float, float, float]], target_x: float, target_y: float, n: int) -> float:
-    '''
 
-    :param table: (x, y, z)
-    :param target_x:
-    :param target_y:
-    :param n:
-    :return:
-    '''
-
+def newton_polynomial2D(
+    table: list[tuple[float, float, float]], target_x: float, target_y: float, degree: int
+) -> float:
     x_values = []
-    y_values = []
-    for x, y, _ in table:
+    for x, _, _ in table:
         if x not in x_values:
             x_values.append(x)
-        if y not in y_values:
-            y_values.append(y)
-
 
     xv_values = []
     for cur_x in x_values:
         yz_values = [(y, z) for x, y, z in table if x == cur_x]
-        xv_values.append((cur_x, newton_polynomial(yz_values, target_y, n)))
+        y_degree = min(degree, len(yz_values) - 1)
+        xv_values.append((cur_x, newton_polynomial(yz_values, target_y, y_degree)))
 
-    return newton_polynomial(xv_values, target_x, n)
-
-
-
-f = lambda x, y: 2 * x**2 + (y - 4)*3 + 3
-
-t = [(x, y, f(x, y)) for x in range(1, 20) for y in range(1, 20)]
-n = 3
-
-x, y = 13.5, 4.2
-
-np_res = newton_polynomial2D(t, x, y, n)
-res = f(x, y)
-
-print(f"np: {np_res}\nres: {res}")
-
+    x_degree = min(degree, len(xv_values) - 1)
+    return newton_polynomial(xv_values, target_x, x_degree)
---
+++ sem_4/compul_algs/lab_01/solver.py
@@ -0,0 +1,129 @@
+from math import pi
+from interpolation import newton_polynomial2D, newton_polynomial
+
+EPS = 1e-5
+TIME_SCALE = 1e-6
+TIME_EPS_FACTOR = 0.5
+
+class Solver:
+    def __init__(self, I_table, Nh_table, sigma_tabe, c_table, q_table):
+        self.I_table = I_table
+        self.Nh_table = Nh_table
+        self.sigma_table = sigma_tabe
+        self.c_table = c_table
+        self.q_table = q_table
+
+        self.n = 3
+        self.solve_p_right: float | None = None
+
+    def I_of(self, t: float) -> float:
+        return newton_polynomial(self.I_table, t, self.n)
+
+    def c_of(self, T: float, p: float) -> float:
+        return newton_polynomial2D(self.c_table, T, p, self.n)
+
+    def q_of(self, T: float, p: float) -> float:
+        return newton_polynomial2D(self.q_table, T, p, self.n)
+
+    def sigma_of(self, T: float, p: float) -> float:
+        return newton_polynomial2D(self.sigma_table, T, p, self.n)
+
+    def Nh_of(self, T: float, p: float) -> float:
+        return newton_polynomial2D(self.Nh_table, T, p, self.n)
+
+    def phi(self, t: float, T: float, p: float) -> float:
+        I = self.I_of(t)
+        j = I / (pi * self.R**2)
+        sigma = self.sigma_of(T, p)
+        q = self.q_of(T, p)
+        c = self.c_of(T, p)
+
+        return (j**2 / sigma - q) / c
+
+    def p_of(self, T: float) -> float:
+        if self.solve_p_right is None:
+            raise ValueError("solve_p_right is not initialized")
+
+        # диапазон поиска корня
+        a, b = 0.3 * 1e6, 2.5 * 10e6
+
+        # функция вычисления корня
+        def f(p: float) -> float:
+            return self.Nh_of(T, p) - self.solve_p_right
+
+        # поиск половинным делением
+        fa = f(a)
+        fb = f(b)
+        if fa == 0:
+            return a
+        if fb == 0:
+            return b
+
+        while b - a > EPS:
+            p = (b + a) / 2
+            fc = f(p)
+            if abs(fc) <= EPS:
+                return p
+            if fa * fc < 0:
+                b = p
+            else:
+                a = p
+                fa = fc
+
+        return (a + b) / 2
+
+    def Rd_of(self, l: float, sigma: float, R: float) -> float:
+        return l / (pi * sigma * R**2)
+
+    def Fr_of(self, q: float, R: float) -> float:
+        return q * R / 2
+
+    def solve(self, t0: float, t_end: float, t_step: float, T0: float, R: float, l: float, px: float):
+        Tx = 300
+        self.R = R
+        self.solve_p_right = 7.242 * 10 ** 4 * px / Tx
+        t = t0 * TIME_SCALE
+        t_end *= TIME_SCALE
+        t_step *= TIME_SCALE
+        T = T0
+        time_eps = t_step * TIME_EPS_FACTOR
+
+        data: dict[str, list[float]] = { "t": [], "T": [], "p": [], "sigma": [], "q": [], "Rd": [], "Fr": [] }
+        while t <= t_end + time_eps:
+            p = self.p_of(T)
+
+            sigma = self.sigma_of(T, p)
+            q = self.q_of(T, p)
+            Rd = self.Rd_of(l, sigma, R)
+            Fr = self.Fr_of(q, R)
+
+            data["t"].append(round(t / TIME_SCALE, 10))
+            data["T"].append(T)
+            data["p"].append(p)
+            data["sigma"].append(sigma)
+            data["q"].append(q)
+            data["Rd"].append(Rd)
+            data["Fr"].append(Fr)
+
+            # полушаг T -> T_n+1/2
+            k1 = self.phi(t, T, p)
+            T_half = T + (t_step / 2) * k1
+            p_half = self.p_of(T_half)
+            k2 = self.phi(t + t_step / 2, T_half, p_half)
+            T = T + t_step * k2
+
+            # переход к временному моменту
+            t += t_step
+
+        return data
+
+
+'''
+На входе:
+t_0
+t_k
+step
+
+T_0
+
+'''
