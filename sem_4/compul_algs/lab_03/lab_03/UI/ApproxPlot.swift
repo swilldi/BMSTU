46f0dd84369a07a6127427475b7c4a555d84b11f
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-03-22 17:17:39 +0300

    задание 1 выполненно

---
+++ sem_4/compul_algs/lab_03/lab_03/UI/ApproxPlot.swift
@@ -0,0 +1,18 @@
+//
+//  ApproxPlot.swift
+//  lab_03
+//
+//  Created by Dmitriy Dudurev on 22.03.2026.
+//
+
+import SwiftUI
+
+struct ApproxPlot: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ApproxPlot()
+}
