184c5c41cdae1d83e39db43996d3f0848dd16d6f
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-05-01 15:39:57 +0300

    add base object

---
+++ sem_4/oop/lab_03/Component/BaseObject.cpp
@@ -0,0 +1,38 @@
+//
+// Created by Dmitriy Dudurev on 01.05.2026.
+//
+
+#include "BaseObject.h"
+
+BaseObject::iterator BaseObject::begin()
+{
+    return iterator();
+}
+
+BaseObject::iterator BaseObject::end()
+{
+    return iterator();
+}
+
+BaseObject::const_iterator BaseObject::begin() const
+{
+    return const_iterator();
+}
+
+BaseObject::const_iterator BaseObject::end() const
+{
+    return const_iterator();
+}
+
+void BaseObject::add(std::shared_ptr<BaseObject> object) { }
+
+std::shared_ptr<BaseObject> BaseObject::get_object(const size_t /*id*/) const
+{
+    return nullptr;
+}
+
+Point BaseObject::get_center() const noexcept
+{
+    return { 0, 0, 0 };
+}
+
---
+++ sem_4/oop/lab_03/Component/BaseObject.h
@@ -0,0 +1,41 @@
+//
+// Created by Dmitriy Dudurev on 01.05.2026.
+//
+
+#ifndef LAB_03_BASEOBJECT_H
+#define LAB_03_BASEOBJECT_H
+
+#include <map>
+#include <memory>
+#include <cstddef>
+
+class BaseObject;
+using mapObjects = std::map<size_t, std::shared_ptr<BaseObject>>;
+
+class BaseObject
+{
+public:
+    using iterator = mapObjects::iterator;
+    using const_iterator = mapObjects::const_iterator;
+
+    BaseObject() = default;
+    virtual ~BaseObject() = default;
+
+    virtual iterator begin();
+    virtual iterator end();
+
+    virtual const_iterator begin() const;
+    virtual const_iterator end() const;
+
+    virtual std::shared_ptr<BaseObject> get_object(const size_t id) const;
+    virtual void add(std::shared_ptr<BaseObject> object);
+    virtual void remove(size_t id) noexcept;
+
+    virtual void accept(std::shared_ptr<BaseVisitor> visitor) = 0;
+
+    virtual Point get_center() const noexcept;
+    virtual bool is_composite() const noexcept = 0;
+    virtual bool is_visible() const noexcept = 0;
+};
+
+#endif //LAB_03_BASEOBJECT_H
