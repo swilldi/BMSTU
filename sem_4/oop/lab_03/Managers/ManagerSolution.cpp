0429b017ac9f91fe8e45be49e53920317636851d
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-05-01 20:01:17 +0300

    add files for manager solution

---
+++ sem_4/oop/lab_03/Managers/ManagerSolution.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dmitriy Dudurev on 01.05.2026.
+//
+
+#include "ManagerSolution.h"
---
+++ sem_4/oop/lab_03/Managers/ManagerSolution.h
@@ -0,0 +1,19 @@
+//
+// Created by Dmitriy Dudurev on 01.05.2026.
+//
+
+#ifndef LAB_03_MANAGERSOLUTION_H
+#define LAB_03_MANAGERSOLUTION_H
+#include <memory>
+
+class ManagerSolution
+{
+public:
+    ManagerSolution() = delete;
+    ~ManagerSolution() = delete;
+    ManagerSolution(ManagerSolution &) = delete;
+
+    // static std::shared_ptr
+};
+
+#endif //LAB_03_MANAGERSOLUTION_H
