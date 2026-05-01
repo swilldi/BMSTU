e6478288cfc8d460e1ca2de5837b4e3f4cc8bb50
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-05-01 18:42:03 +0300

    add BaseModel

---
+++ sem_4/oop/lab_03/Component/SceneObject/Model/BaseModel.h
@@ -0,0 +1,27 @@
+//
+// Created by Dmitriy Dudurev on 01.05.2026.
+//
+
+#ifndef LAB_03_BASEMODEL_H
+#define LAB_03_BASEMODEL_H
+#include "Component/SceneObject/SceneObject.h"
+
+
+class BaseModel : public SceneObject
+{
+public:
+    BaseModel() = default;
+
+    explicit BaseModel(std::shared_ptr<BaseStructure> structure) : _structure(structure) {}
+
+    virtual ~BaseModel() override = default;
+
+    virtual std::shared_ptr<BaseStructure> get_structure() const = 0;
+
+    bool is_visible() const noexcept override { return true; }
+
+protected:
+    std::shared_ptr<BaseStructure> _structure;
+};
+
+#endif //LAB_03_BASEMODEL_H
