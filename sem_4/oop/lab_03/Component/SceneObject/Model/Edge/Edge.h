cec587ad4c1e2b88199b637638a1cb193a947636
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-05-01 18:30:40 +0300

    add edge

---
+++ sem_4/oop/lab_03/Component/SceneObject/Model/Edge/Edge.h
@@ -0,0 +1,31 @@
+//
+// Created by Dmitriy Dudurev on 01.05.2026.
+//
+
+#ifndef LAB_03_EDGE_H
+#define LAB_03_EDGE_H
+
+#include <cstddef>
+
+class Edge
+{
+public:
+    Edge() = default;
+    Edge(size_t from, size_t to);
+
+    Edge(const Edge &other) = default;
+    Edge &operator=(const Edge &other) = default;
+    Edge(Edge &&other) = default;
+    Edge &operator=(Edge &&other) = default;
+
+    size_t get_from();
+    size_t get_to();
+
+    void set_from(size_t from);
+    void set_to(size_t to);
+
+private:
+    size_t _from, _to;
+};
+
+#endif //LAB_03_EDGE_H
