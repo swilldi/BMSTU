bd1c4e1e2c965442cf778629472e48fe27dd8f8f
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-05-01 22:27:44 +0300

    add move action

---
+++ sem_4/oop/lab_03/Transform/Action/Move/MoveAction.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Dmitriy Dudurev on 01.05.2026.
+//
+
+#include "MoveAction.h"
+#include "Transform/Data/Move/MoveData.h"
+
+MoveAction::MoveAction(const MoveData &data) noexcept : TransformAcction()
+{
+    _matrix[0][3] = data.get_dx();
+    _matrix[1][3] = data.get_dy();
+    _matrix[2][3] = data.get_dz();
+}
+
+bool MoveAction::is_move_action() const noexcept
+{
+    return true;
+}
+
---
+++ sem_4/oop/lab_03/Transform/Action/Move/MoveAction.h
@@ -0,0 +1,20 @@
+//
+// Created by Dmitriy Dudurev on 01.05.2026.
+//
+
+#ifndef LAB_03_MOVEACTION_H
+#define LAB_03_MOVEACTION_H
+#include "Transform/Action/TransformAction.h"
+#include "Transform/Data/Move/MoveData.h"
+
+class MoveAction : public TransformAcction
+{
+public:
+    MoveAction() = default;
+    virtual ~MoveAction() = default;
+    MoveAction(const MoveData &data) noexcept;
+
+    bool is_move_action() const noexcept override;
+};
+
+#endif //LAB_03_MOVEACTION_H
