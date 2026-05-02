0b194b25a95f5e381d28a6f96ceef57879b7b9cd
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-05-02 17:42:20 +0300

    add manager solution

--- sem_4/oop/lab_03/Managers/ManagerSolution.h
+++ sem_4/oop/lab_03/Managers/ManagerSolution.h
@@ -4,6 +4,12 @@
 
 #ifndef LAB_03_MANAGERSOLUTION_H
 #define LAB_03_MANAGERSOLUTION_H
+#include "Camera/CameraManager.h"
+#include "Draw/DrawManager.h"
+#include "Load/LoadManager.h"
+#include "Scene/SceneManager.h"
+#include "Transform/TransformManager.h"
+
 #include <memory>
 
 class ManagerSolution
@@ -13,7 +19,12 @@ public:
     ~ManagerSolution() = delete;
     ManagerSolution(ManagerSolution &) = delete;
 
-    // static std::shared_ptr
+    static std::shared_ptr<CameraManager> get_camera_manager();
+    static std::shared_ptr<DrawManager> get_draw_manager();
+    static std::shared_ptr<LoadManager> get_load_manager();
+    static std::shared_ptr<SceneManager> get_scene_manager();
+    static std::shared_ptr<TransformManager> get_transform_manager();
+
 };
 
 #endif //LAB_03_MANAGERSOLUTION_H
