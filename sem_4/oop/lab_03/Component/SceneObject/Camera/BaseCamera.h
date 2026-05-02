6564a3d8ef0c7994c8711a4926b3a8ee7794babe
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-05-02 10:40:46 +0300

    Add Camera

---
+++ sem_4/oop/lab_03/Component/SceneObject/Camera/BaseCamera.h
@@ -0,0 +1,23 @@
+//
+// Created by Dmitriy Dudurev on 02.05.2026.
+//
+
+#ifndef LAB_03_BASECAMERA_H
+#define LAB_03_BASECAMERA_H
+#include "Component/SceneObject/SceneObject.h"
+#include "Transform/Action/TransformAction.h"
+
+class BaseCamera : public SceneObject
+{
+public:
+    BaseCamera() = default;
+    virtual ~BaseCamera() override = default;
+
+    virtual const Matrix<double> get_look_matrix() const noexcept = 0;
+    virtual const Matrix<double> get_projection_matrix(double ration) const = 0;
+    virtual void transform(const std::shared_ptr<const TransformAcction> action) = 0;
+
+    bool is_visible() const noexcept override { return false; }
+};
+
+#endif //LAB_03_BASECAMERA_H
---
+++ sem_4/oop/lab_03/Component/SceneObject/Camera/Default/DefaultCamera.h
@@ -0,0 +1,34 @@
+//
+// Created by Dmitriy Dudurev on 02.05.2026.
+//
+
+#ifndef LAB_03_DEFAULTCAMERA_H
+#define LAB_03_DEFAULTCAMERA_H
+
+#include "Component/SceneObject/Camera/BaseCamera.h"
+#include "Vector3D/Vector3D.h"
+
+class DefaultCamera : public BaseCamera
+{
+public:
+    DefaultCamera();
+    DefaultCamera(const Point &point);
+
+    virtual ~DefaultCamera() override = default;
+
+    const Matrix<double> get_look_matrix() const noexcept override;
+    const Matrix<double> get_projection_matrix(double ratio) const override;
+    Point get_center() const noexcept override;
+
+    void transform(const std::shared_ptr<const TransformAcction> action) override;
+    void accept(std::shared_ptr<BaseVisitor> visitor) override;
+
+private:
+    Point _viewpoint;
+    Vector3D<double> _up, _right, _forward;
+    double _fov, _near, _far;
+
+
+};
+
+#endif //LAB_03_DEFAULTCAMERA_H
