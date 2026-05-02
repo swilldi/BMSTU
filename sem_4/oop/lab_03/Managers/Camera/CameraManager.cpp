96cdbc3ccb3a6bab9fe9db2dcfe4b1af57f50a64
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-05-02 17:40:26 +0300

    add camera manager

--- sem_4/oop/lab_03/Managers/Camera/CameraManager.cpp
+++ sem_4/oop/lab_03/Managers/Camera/CameraManager.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "CameraManager.h"
+
+#include "Factories/Camera/CameraFactory.h"
+#include "Managers/ManagerSolution.h"
+
+void CameraManager::set_active_camera(size_t id)
+{
+    auto scene_manager = ManagerSolution::get_scene_manager();
+    auto camera_obj = scene_manager->get_object(id);
+    auto camera = std::dynamic_pointer_cast<BaseCamera>(camera_obj);
+
+    if (!camera)
+        throw nullptr;
+
+    _active_camera = camera;
+    _active_camera_id = id;
+
+    auto draw_manager = ManagerSolution::get_draw_manager();
+    draw_manager->draw();
+}
+
+std::shared_ptr<BaseCamera> CameraManager::get_active_camera() const noexcept
+{
+    return _active_camera;
+}
+
+void CameraManager::add_camera(const CameraID &id)
+{
+    CameraFactory factory;
+    auto camera = factory.create(id);
+
+    auto scene_manager = ManagerSolution::get_scene_manager();
+    scene_manager->add_object(camera);
+}
+
+void CameraManager::remove_camera(size_t id)
+{
+    auto scene_manager = ManagerSolution::get_scene_manager();
+    scene_manager->remove_object(id);
+}
