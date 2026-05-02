a06e6ed32c9795a36d279ee7ca20c4714222fe80
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-05-02 18:39:20 +0300

    add director solution

---
+++ sem_4/oop/lab_03/Director/DirectorSolution.h
@@ -0,0 +1,38 @@
+//
+// Created by Dmitriy Dudurev on 02.05.2026.
+//
+
+#ifndef LAB_03_DIRECTORSOLUTION_H
+#define LAB_03_DIRECTORSOLUTION_H
+#include "BaseDirector.h"
+
+#include <functional>
+#include <map>
+#include <Concepts/Concepts.h>
+
+template <typename... SupportedArgs>
+class DirectorSolution
+{
+public:
+    DirectorSolution() = default;
+
+    ~DirectorSolution() = default;
+
+    template <Derivative<BaseDirector> Derived, typename... Args> requires
+        (IsSupportedArgs<Args, SupportedArgs...> && ...) && ConstructableWith<Derived, Args...>
+    void registrate(size_t id);
+
+    template <typename ... Args> requires (IsSupportedArgs<Args, SupportedArgs...> && ...)
+    std::unique_ptr<BaseDirector> create(size_t id, Args && ...);
+
+private:
+    using CreatorFunction = std::function<std::unique_ptr<BaseDirector>(
+        const std::tuple<SupportedArgs...> &)>;
+    std::map<size_t, CreatorFunction> _creators;
+};
+
+#include "DirectorSolution.hpp"
+
+using BaseDirectorSolution = DirectorSolution<std::shared_ptr<BaseBuilder>>;
+
+#endif //LAB_03_DIRECTORSOLUTION_H
