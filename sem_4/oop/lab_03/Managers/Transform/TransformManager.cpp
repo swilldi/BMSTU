ac1b4b9e221f1ae3534ec2e1a082ed878314a671
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-05-02 18:09:34 +0300

    add transform manager

--- sem_4/oop/lab_03/Managers/Transform/TransformManager.cpp
+++ sem_4/oop/lab_03/Managers/Transform/TransformManager.cpp
@@ -3,3 +3,36 @@
 //
 
 #include "TransformManager.h"
+
+#include "Managers/ManagerSolution.h"
+#include "Visitor/Transform/Move/MoveVisitor.h"
+#include "Visitor/Transform/Rotate/RotateVisitor.h"
+#include "Visitor/Transform/Scale/ScaleVisitor.h"
+
+void TransformManager::move_object(size_t id, const MoveData &data)
+{
+    auto scene_manger = ManagerSolution::get_scene_manager();
+    auto object = scene_manger->get_object(id);
+
+    auto visitor = std::make_shared<MoveVisitor>(data);
+    object->accept(visitor);
+}
+
+void TransformManager::rotate_object(size_t id, const RotateData &data)
+{
+    auto scene_manger = ManagerSolution::get_scene_manager();
+    auto object = scene_manger->get_object(id);
+
+    auto visitor = std::make_shared<RotateVisitor>(data);
+    object->accept(visitor);
+}
+
+void TransformManager::scale_object(size_t id, const ScaleData &data)
+{
+    auto scene_manger = ManagerSolution::get_scene_manager();
+    auto object = scene_manger->get_object(id);
+
+    auto visitor = std::make_shared<ScaleVisitor>(data);
+    object->accept(visitor);
+}
+
--- sem_4/oop/lab_03/Managers/Transform/TransformManager.h
+++ sem_4/oop/lab_03/Managers/Transform/TransformManager.h
@@ -4,5 +4,20 @@
 
 #ifndef LAB_03_TRANSFORMMANAGER_H
 #define LAB_03_TRANSFORMMANAGER_H
+#include "Managers/BaseManager.h"
+#include "Transform/Data/Move/MoveData.h"
+#include "Transform/Data/Rotate/RotateData.h"
+#include "Transform/Data/Scale/ScaleData.h"
+
+class TransformManager : public BaseManager
+{
+public:
+    TransformManager() = default;
+    virtual ~TransformManager() = default;
+
+    void move_object(size_t id, const MoveData &data);
+    void rotate_object(size_t id, const RotateData &data);
+    void scale_object(size_t id, const ScaleData &data);
+};
 
 #endif //LAB_03_TRANSFORMMANAGER_H
