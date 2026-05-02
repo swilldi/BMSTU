3cab37ec3e6d7be2f81aae8d3929b3abba7f3575
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-05-02 14:05:11 +0300

    add draw visitor

---
+++ sem_4/oop/lab_03/Visitor/Draw/DrawVisitor.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Dmitriy Dudurev on 02.05.2026.
+//
+
+#include "DrawVisitor.h"
+
+DrawVisitor::DrawVisitor(std::shared_ptr<BaseProjection> projection,
+                         std::shared_ptr<BaseRemoveInvisibleEdge> invisible_edge,
+                         std::shared_ptr<BaseCoordinateConvert> coordinate_convert,
+                         std::shared_ptr<BasePainter> painter,
+                         std::shared_ptr<BaseCamera> camera) : _projection(projection),
+                                                               _invisible_edge(invisible_edge),
+                                                               _coordinate_convert(coordinate_convert),
+                                                               _painter(painter), _camera(camera) {}
+
+void DrawVisitor::visit(BaseCamera &/*camera*/) const { }
---
+++ sem_4/oop/lab_03/Visitor/Draw/DrawVisitor.h
@@ -0,0 +1,35 @@
+//
+// Created by Dmitriy Dudurev on 02.05.2026.
+//
+
+#ifndef LAB_03_DRAWVISITOR_H
+#define LAB_03_DRAWVISITOR_H
+#include "Strategies/CoordinateConvert/BaseCoordinateConvert.h"
+#include "Strategies/Projection/BaseProjection.h"
+#include "Strategies/RemoveInvisibleEdge/BaseRemoveInvisibleEdge.h"
+#include "Visitor/BaseVisitor.h"
+
+class DrawVisitor : public BaseVisitor
+{
+public:
+    DrawVisitor() = delete;
+    DrawVisitor(std::shared_ptr<BaseProjection> projection, std::shared_ptr<BaseRemoveInvisibleEdge> invisible_edge,
+              std::shared_ptr<BaseCoordinateConvert> coordinate_convert, std::shared_ptr<BasePainter> painter,
+              std::shared_ptr<BaseCamera> camera);
+
+    virtual ~DrawVisitor() override = default;
+
+    void visit(BaseCamera &camera) const override;
+    void visit(std::shared_ptr<BaseStructure> &structure) const override;
+
+private:
+    std::shared_ptr<BasePainter> _painter;
+    std::shared_ptr<BaseCamera> _camera;
+
+    std::shared_ptr<BaseProjection> _projection;
+    std::shared_ptr<BaseRemoveInvisibleEdge> _invisible_edge;
+    std::shared_ptr<BaseCoordinateConvert> _coordinate_convert;
+
+};
+
+#endif //LAB_03_DRAWVISITOR_H
