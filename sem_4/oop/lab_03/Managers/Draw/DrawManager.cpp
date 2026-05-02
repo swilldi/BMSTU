e3d233abace984d1b996c5fa7fccaec7cf896339
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-05-02 17:40:33 +0300

    add draw manager

--- sem_4/oop/lab_03/Managers/Draw/DrawManager.cpp
+++ sem_4/oop/lab_03/Managers/Draw/DrawManager.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "DrawManager.h"
+
+#include "Managers/ManagerSolution.h"
+#include "Strategies/CoordinateConvert/Creator/CoordinateConvertCreator.h"
+#include "Strategies/Projection/Creator/ProjectionCreator.h"
+#include "Strategies/RemoveInvisibleEdge/Creator/RemoveInvisibleEdgeCreator.h"
+#include "Visitor/Draw/DrawVisitor.h"
+
+void DrawManager::set_drawer(std::shared_ptr<BaseDrawer> drawer)
+{
+    _drawer = drawer;
+}
+
+void DrawManager::draw()
+{
+    _drawer->clear();
+
+    auto camera_manager = ManagerSolution::get_camera_manager();
+    auto active_camera = camera_manager->get_active_camera();
+
+    auto projection = DefaultProjectionCreator::create();
+    auto remove_invisible = RayCastingRemoveInvisibleEdgeCreator::create();
+    auto coordinate_convert = DefaultCoordinateConvertCreator::create();
+
+    auto draw_visitor = std::make_shared<DrawVisitor>(std::move(projection), std::move(remove_invisible),
+                                                      std::move(coordinate_convert), _drawer, active_camera);
+
+    auto scene_manager = ManagerSolution::get_scene_manager();
+    scene_manager->accept(draw_visitor);
+}
--- sem_4/oop/lab_03/Managers/Draw/DrawManager.h
+++ sem_4/oop/lab_03/Managers/Draw/DrawManager.h
@@ -4,5 +4,22 @@
 
 #ifndef LAB_03_DRAWMANAGER_H
 #define LAB_03_DRAWMANAGER_H
+#include "Factories/Draw/Products/BaseDrawer.h"
+#include "Managers/BaseManager.h"
+
+#include <memory>
+
+class DrawManager : public BaseManager
+{
+public:
+    DrawManager() = default;
+    virtual ~DrawManager() override = default;
+
+    void set_drawer(std::shared_ptr<BaseDrawer> drawer);
+    void draw();
+
+private:
+    std::shared_ptr<BaseDrawer> _drawer;
+};
 
 #endif //LAB_03_DRAWMANAGER_H
