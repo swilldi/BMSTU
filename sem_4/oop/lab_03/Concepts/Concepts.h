9bbe49512972177055653a3f808219eca0238b7d
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-05-02 09:37:37 +0300

    update concepts

--- sem_4/oop/lab_03/Concepts/Concepts.h
+++ sem_4/oop/lab_03/Concepts/Concepts.h
@@ -7,6 +7,18 @@
 
 #include <concepts>
 
+template <typename Derive, typename Base>
+concept Derivative = std::is_abstract_v<Base> && std::is_base_of_v<Base, Derive>;
+
+template <typename T, typename ... Args>
+concept ConstructableWith = requires(Args && ... args)
+{
+    T { std::forward<Args>(args)... };
+};
+
+template <typename T, typename ... Args>
+concept IsSupportedArgs = (std::is_convertible_v<T, Args> || ...);
+
 template <typename T>
 concept ConvertibleToDouble = std::convertible_to<T, double>;
 
