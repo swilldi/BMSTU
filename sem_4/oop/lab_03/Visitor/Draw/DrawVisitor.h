c68c7d6dc33f854d009e0472ae515a89fbf22490
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-05-02 17:41:27 +0300

    any rename

--- sem_4/oop/lab_03/Visitor/Draw/DrawVisitor.h
+++ sem_4/oop/lab_03/Visitor/Draw/DrawVisitor.h
@@ -4,6 +4,7 @@
 
 #ifndef LAB_03_DRAWVISITOR_H
 #define LAB_03_DRAWVISITOR_H
+#include "Factories/Draw/Products/BaseDrawer.h"
 #include "Strategies/CoordinateConvert/BaseCoordinateConvert.h"
 #include "Strategies/Projection/BaseProjection.h"
 #include "Strategies/RemoveInvisibleEdge/BaseRemoveInvisibleEdge.h"
@@ -14,7 +15,7 @@ class DrawVisitor : public BaseVisitor
 public:
     DrawVisitor() = delete;
     DrawVisitor(std::shared_ptr<BaseProjection> projection, std::shared_ptr<BaseRemoveInvisibleEdge> invisible_edge,
-              std::shared_ptr<BaseCoordinateConvert> coordinate_convert, std::shared_ptr<BasePainter> painter,
+              std::shared_ptr<BaseCoordinateConvert> coordinate_convert, std::shared_ptr<BaseDrawer> drawer,
               std::shared_ptr<BaseCamera> camera);
 
     virtual ~DrawVisitor() override = default;
@@ -23,7 +24,7 @@ public:
     void visit(std::shared_ptr<BaseStructure> &structure) const override;
 
 private:
-    std::shared_ptr<BasePainter> _painter;
+    std::shared_ptr<BaseDrawer> _painter;
     std::shared_ptr<BaseCamera> _camera;
 
     std::shared_ptr<BaseProjection> _projection;
