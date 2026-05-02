2ce3535d23aa6ca05efc37baf2bc5958c15d0c65
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-05-02 19:03:29 +0300

    add load manager

--- sem_4/oop/lab_03/Managers/Load/LoadManager.h
+++ sem_4/oop/lab_03/Managers/Load/LoadManager.h
@@ -4,5 +4,21 @@
 
 #ifndef LAB_03_LOADMANAGER_H
 #define LAB_03_LOADMANAGER_H
+#include "Director/DirectorSolution.h"
+#include "Managers/BaseManager.h"
+#include "ID/ID.h"
+#include <string>
+
+class LoadManager : public BaseManager
+{
+public:
+    LoadManager();
+    virtual ~LoadManager() override = default;
+
+    void load(ModelRepresentationID representation_id, const std::string &filename);
+
+private:
+    std::shared_ptr<BaseDirectorSolution> _director_solution;
+};
 
 #endif //LAB_03_LOADMANAGER_H
