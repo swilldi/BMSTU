63bf3cf446ec5cd6a9e9da723ce700d4ef167a01
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-05-03 08:39:20 +0300

    add load command

--- sem_4/oop/lab_03/Commands/Load/LoadCommand.cpp
+++ sem_4/oop/lab_03/Commands/Load/LoadCommand.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "LoadCommand.h"
+
+#include "Managers/ManagerSolution.h"
+
+BoneLoadCommand::BoneLoadCommand(ModelRepresentationID representation_id,const std::pmr::string &filename) :
+    _action(&LoadManager::load), _representation_id(representation_id), _filename(filename),
+    _load_manager(ManagerSolution::get_load_manager()) { }
+
+void BoneLoadCommand::execute()
+{
+    (*_load_manager.*_action)(_representation_id, _filename);
+}
