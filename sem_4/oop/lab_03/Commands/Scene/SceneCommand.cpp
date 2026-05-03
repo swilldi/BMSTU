2b4738ebbbc2380bf4fd5f14cdbf0185a542732f
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-05-03 09:23:10 +0300

    add scene command

--- sem_4/oop/lab_03/Commands/Scene/SceneCommand.cpp
+++ sem_4/oop/lab_03/Commands/Scene/SceneCommand.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "SceneCommand.h"
+
+#include "Managers/ManagerSolution.h"
+
+DrawSceneCommand::DrawSceneCommand() :
+    _action(&DrawManager::draw), _draw_manager(ManagerSolution::get_draw_manager()) { }
+
+void DrawSceneCommand::execute()
+{
+    (*_draw_manager.*_action)();
+}
