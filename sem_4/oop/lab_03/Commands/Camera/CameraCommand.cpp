e4f37dd22844925dc07eee19d9221f274f9e12c7
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-05-03 08:21:01 +0300

    add camera command

--- sem_4/oop/lab_03/Commands/Camera/CameraCommand.cpp
+++ sem_4/oop/lab_03/Commands/Camera/CameraCommand.cpp
@@ -3,3 +3,4 @@
 //
 
 #include "CameraCommand.h"
+A
--- sem_4/oop/lab_03/Commands/Camera/CameraCommand.h
+++ sem_4/oop/lab_03/Commands/Camera/CameraCommand.h
@@ -5,7 +5,12 @@
 #ifndef LAB_03_CAMERACOMMAND_H
 #define LAB_03_CAMERACOMMAND_H
 #include "Commands/BaseCommand.h"
+#include "Managers/Camera/CameraManager.h"
+#include "ID/ID.h"
+#include "Managers/Transform/TransformManager.h"
+#include "Transform/Data/Move/MoveData.h"
 
+// === Базовый класс ===
 class CameraCommand : public BaseCommand
 {
 public:
@@ -13,19 +18,98 @@ public:
     virtual ~CameraCommand() = default;
 };
 
+
+// === Добавление камеры ===
 class AddCameraCommand final : public CameraCommand
 {
-    using Action = void (CameraManager::*)(const CameraId &);
-
 public:
-    AddCameraCommand(const CameraId &id);
+    AddCameraCommand(const CameraID &id);
     virtual ~AddCameraCommand() override = default;
 
     void execute() override;
+
 private:
+    using Action = void (CameraManager::*)(const CameraID &);
+
     Action _action;
-    CameraId _id;
+    CameraID _id;
     std::shared_ptr<CameraManager> _camera_manager;
 };
+
+
+// === Удаление камеры ===
+class RemoveCameraCommand : public CameraCommand
+{
+public:
+    RemoveCameraCommand(const CameraID &id);
+    virtual ~RemoveCameraCommand() override = default;
+
+    void execute() override;
+
+private:
+    using Action = void (CameraManager::*)(const CameraID &);
+
+    Action _action;
+    CameraID _id;
+    std::shared_ptr<CameraManager> _camera_manager;
+
+};
+
+
+// === Выбор камеры ===
+class SetCameraCommand : public CameraCommand
+{
+public:
+    SetCameraCommand(const CameraID &id);
+    virtual ~SetCameraCommand() override = default;
+
+    void execute() override;
+
+private:
+    using Action = void (CameraManager::*)(const CameraID &);
+
+    Action _action;
+    CameraID _id;
+    std::shared_ptr<CameraManager> _camera_manager;
+};
+
+
+// === Перемещение камеры ===
+class MoveCameraCommand : public CameraCommand
+{
+public:
+    MoveCameraCommand(const CameraID &id, const MoveData &data);
+    virtual ~MoveCameraCommand() override = default;
+
+    void execute() override;
+
+private:
+    using Action = void (CameraManager::*)(const CameraID &, const MoveData &);
+
+    Action _action;
+    CameraID _id;
+    MoveData _data;
+    std::shared_ptr<TransformManager> _transform_manager;
+};
+
+
+// === Вращение камеры ===
+class RotateCameraCommand : public CameraCommand
+{
+public:
+    RotateCameraCommand(const CameraID &id, const RotateData &data);
+    virtual ~RotateCameraCommand() override = default;
+
+    void execute() override;
+
+private:
+    using Action = void (CameraManager::*)(const CameraID &, const RotateData &);
+
+    Action _action;
+    CameraID _id;
+    MoveData _data;
+    std::shared_ptr<TransformManager> _transform_manager;
+};
+
 
 #endif //LAB_03_CAMERACOMMAND_H
