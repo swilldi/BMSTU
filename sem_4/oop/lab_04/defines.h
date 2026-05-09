3012aeb32a3bff25e069bcf2d9545ca42719032a
Dmitriy Dudurev <ddimitruo@gmail.com>
2026-05-09 17:54:18 +0300

    add defines

---
+++ sem_4/oop/lab_04/defines.h
@@ -0,0 +1,23 @@
+//
+// Created by Dmitriy Dudurev on 09.05.2026.
+//
+
+#ifndef LAB_04_DEFINES_H
+#define LAB_04_DEFINES_H
+
+#include <cstddef>
+
+enum Direction : size_t
+{
+    DOWN,
+    STAY,
+    UP
+};
+
+enum CabinID : size_t
+{
+    FIRST,
+    SECOND
+};
+
+#endif //LAB_04_DEFINES_H
